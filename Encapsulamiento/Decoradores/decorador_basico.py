e03cc6a015ce132ce65e55d48153657b6769197e
Mateo Congo <aidenperse@gmail.com>
2024-04-07 12:14:14 -0500

    colocando bien el nombre de la función

--- Encapsulamiento/Decoradores/decorador_basico.py
+++ Encapsulamiento/Decoradores/decorador_basico.py
@@ -1,14 +1,14 @@
 # Definimos un decorador básico que añade funcionalidad antes de llamar a la función original
 def decorardor(funcion):
     # Definimos una nueva función que envuelve la función original
-    def duncion_modificada():
+    def funcion_modificada():
         # Agregamos lógica antes de llamar a la función original
         print("Antes de llamar a la función")
         # Llamamos a la función original
         funcion()
 
     # Devolvemos la nueva función modificada
-    return duncion_modificada
+    return funcion_modificada
 
 
 # Definimos una función saludo y aplicamos el decorador
